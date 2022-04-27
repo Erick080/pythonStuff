0f42a44d802a90fa99fa81281f3541308eb4c9fb
Erick080 <102190004+Erick080@users.noreply.github.com>
2022-04-27 16:30:08 -0300

    Create alistamento.py

---
+++ alistamento.py
@@ -0,0 +1,16 @@
+from datetime import date
+
+ano = date.today().year
+nasc = int (input ('Digite o ano de nascimento:'))
+idade = int (ano - nasc)
+
+if idade < 18:
+    dif = int (18 - idade)
+    print ('Ainda falta[m] {} ano[s] para o alistamento, em {}.' .format (dif, ano + dif))
+
+elif idade > 18:
+    dif = int (idade - 18)
+    print ('O alistamento está {} ano[s] atrasado[s], deveria ter sido feito em {}.' .format (dif, ano - dif))
+
+elif idade == 18:
+    print ('O alistamento deve ser feito este ano')
